5cf734d94d5ab3526a330bb5eb8916f0f942c5b5
MezzMar <Gary.n.1999@gmail.com>
2018-10-14 20:35:57 -0400

    Updates

---
+++ precache-manifest.bd3cfcf06930743a0239ac3ec466c731.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa1c2e8b6e30c61d650214564da8ac4f",
+    "url": "/entornodomestico/static/media/entorno_logo.aa1c2e8b.jpg"
+  },
+  {
+    "revision": "8a0de117ef4106c327f2c047f6cee8e9",
+    "url": "/entornodomestico/static/media/entorno_animacion.8a0de117.gif"
+  },
+  {
+    "revision": "836fa7b4ede07b8b380f00b0dfe09d70",
+    "url": "/entornodomestico/static/media/Fondo_info.836fa7b4.jpg"
+  },
+  {
+    "revision": "8ad353171aa0e786471b",
+    "url": "/entornodomestico/static/js/runtime~main.8ad35317.js"
+  },
+  {
+    "revision": "280660780985ba49d878",
+    "url": "/entornodomestico/static/js/main.28066078.chunk.js"
+  },
+  {
+    "revision": "8498ba2aa9809bef3538",
+    "url": "/entornodomestico/static/js/1.8498ba2a.chunk.js"
+  },
+  {
+    "revision": "280660780985ba49d878",
+    "url": "/entornodomestico/static/css/main.6dfe1773.chunk.css"
+  },
+  {
+    "revision": "ed1bf5083ba421657b7c7f91494cc6e9",
+    "url": "/entornodomestico/index.html"
+  }
+];
